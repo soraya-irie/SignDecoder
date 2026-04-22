56a30594215fd8f9d22055fdbbcf16ab9672f798
soraya-ire <rujiangkongshi@gmail.com>
2026-04-22 21:40:47 +0900

    Initial Commit

---
+++ SignDecoder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SignDecoder
+//
+//  Created by soraya irie on 2026/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SignDecoder/SignDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignDecoderApp.swift
+//  SignDecoder
+//
+//  Created by soraya irie on 2026/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SignDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
