ad1b64f4c9e54c7d84c8b4eb2ee89293e9328511
soraya-ire <rujiangkongshi@gmail.com>
2026-04-24 14:46:30 +0900

    Complete Section2 Step2 - extract text from images

--- SignDecoder/TranslationView.swift
+++ SignDecoder/TranslationView.swift
@@ -2,7 +2,15 @@ import SwiftUI
 
 struct TranslationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Caution, falling rocks")
+
+            Button {
+
+            } label: {
+                Text("Translate")
+            }
+        }
     }
 }
 
