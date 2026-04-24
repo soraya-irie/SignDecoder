b5bfee422e4e02049d5b4f2a4b81d0455417e022
soraya-ire <rujiangkongshi@gmail.com>
2026-04-24 14:56:56 +0900

    Complete Section2 Step5 - extract text from images

--- SignDecoder/TranslationView.swift
+++ SignDecoder/TranslationView.swift
@@ -5,6 +5,13 @@ struct TranslationView: View {
 
     var body: some View {
         VStack {
+            Text("Identified Text")
+                .font(.subheadline.bold())
+                .textCase(.uppercase)
+                .foregroundStyle(.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
+
             Text(text)
                 .frame(maxWidth: .infinity,
                        maxHeight: 50,
