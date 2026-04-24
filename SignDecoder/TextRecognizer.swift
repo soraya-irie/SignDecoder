314e09f242cfb319030a44f8e503127e790af8e9
soraya-ire <rujiangkongshi@gmail.com>
2026-04-25 01:32:58 +0900

    Complete Section4 Step7 - extract text from images

--- SignDecoder/TextRecognizer.swift
+++ SignDecoder/TextRecognizer.swift
@@ -3,12 +3,13 @@ import SwiftUI
 import Vision
 
 struct TextRecognizer {
-    init(imageResource: ImageResource) {
+    init(imageResource: ImageResource) async {
         var request = RecognizeTextRequest()
 
         let image = UIImage(resource: imageResource)
 
-        if let imageData = image.pngData() {
+        if let imageData = image.pngData(),
+           let results = try? await request.perform(on: imageData) {
 
         }
     }
