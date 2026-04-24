9a5b76cdb53b63b06b84862468b74edfee2d74d1
soraya-ire <rujiangkongshi@gmail.com>
2026-04-25 00:33:45 +0900

    Complete Section3 Step2 - extract text from images

--- SignDecoder/TextRecognitionView.swift
+++ SignDecoder/TextRecognitionView.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 
 struct TextRecognitionView: View {
+    let imageResource: ImageResource = .sign1
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(imageResource)
+        }
     }
 }
 
