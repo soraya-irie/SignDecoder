7f872755475deecb84931701357a6f2dc055a4a4
soraya-ire <rujiangkongshi@gmail.com>
2026-04-25 20:38:22 +0900

    Complete Section7 Step4 - extract text from images

--- SignDecoder/BoundsRect.swift
+++ SignDecoder/BoundsRect.swift
@@ -6,6 +6,7 @@ struct BoundsRect: Shape {
     let normalizedRect: NormalizedRect
 
     func path(in rect: CGRect) -> Path {
-
+        let imageCoordinatesRect = normalizedRect.toImageCoordinates(rect.size, origin: .upperLeft)
+        return Path(imageCoordinatesRect)
     }
 }
