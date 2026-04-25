9af8d84c5d4fbff1c05c3645c9f509b0863344bd
soraya-ire <rujiangkongshi@gmail.com>
2026-04-25 20:03:08 +0900

    Complete Section6 Step3 - extract text from images

--- SignDecoder/ImageGalleryView.swift
+++ SignDecoder/ImageGalleryView.swift
@@ -3,6 +3,12 @@ import SwiftUI
 struct ImageGalleryView: View {
     let itemSize = 100.0
 
+    let imageResources: [[ImageResource]] = [
+        [.sign1, .sign2, .sign3],
+        [.sign4, .sign5, .sign6],
+        [.sign7, .sign8, .sign9]
+    ]
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
